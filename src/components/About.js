f12f963e5d06756a16b659d2e2dc51e2f83c9cb5
Azani David Pinkney <info@azani.org>
2023-11-17 20:15:37 -0800

    props lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,15 @@
 import React from "react";
 
-function About() {
+
+
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {props.bio && props.bio !== "" ? <p>{props.bio}</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <a href={props.links.github}>Github</a>
+      <a href={props.links.linkedin}>LinkedIn</a>
     </div>
   );
 }
